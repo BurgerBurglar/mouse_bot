aed5658fc7fac1796db910c681bc55615bc97568
Shuo Tian <tianshuo1996@outlook.com>
2021-10-10 00:44:17 -0500

    fixed bug that could affect kicking dongdong out

--- kickDongdong.ts
+++ kickDongdong.ts
@@ -5,6 +5,7 @@ const votesNeeded = 3
 let voteStarted = false
 let recentMessages: { "userId": string, "text": string }[] = []
 const keywords = ["信用卡", "银行", "存款", "体制", "客户", "网点", "存款", "资产", "签证", "理财", "营业", "编制", "事业编", "贷款", "公积金", "对公", "工行", "营销", "工银"]
+const dongdongName = "栋栋🤧"
 
 const isKickable = (text: string) => {
     if (text.length > 15 * 7) return true
@@ -18,15 +19,15 @@ const isKickable = (text: string) => {
 const canKickDongdong = async (msg: Message) => {
     const room = msg.room()
     if (room === null || await room?.topic() !== "皇马upup") return false
-    const dongdong = await room.member("栋栋🤧")
+    const dongdong = await room.member(dongdongName)
     if (dongdong === null) return false
-    return dongdong
+    return true
 }
 const startVote = async (msg: Message) => {
     if (!canKickDongdong(msg)) return false
     const text = getMessageText(msg)
     if (text === null) return false
-    if (isKickable(text) && msg.talker().name() == "栋栋🤧") {
+    if (isKickable(text) && msg.talker().name() == dongdongName) {
         msg.say('机器人检测到栋栋的发言存在骚扰行为，是否要把栋栋踢出此群？回复"踢了吧"可参与投票。#踢栋机器人')
         if (!voteStarted)
             voteStarted = true
@@ -35,7 +36,8 @@ const startVote = async (msg: Message) => {
     return false
 }
 const kickDongdong = async (msg: Message) => {
-    const dongdong = await canKickDongdong(msg)
+    if (!await canKickDongdong(msg)) return
+    const dongdong = await msg.room()!.member(dongdongName)
     if (!dongdong) return
     msg.say("李祥栋你好，你的发言存在骚扰行为，根据民主投票，我代表党和人民踢你出群。#踢栋机器人")
     await Wechaty.sleep(3 * 1000)
@@ -61,7 +63,7 @@ const readVotes = async (msg: Message) => {
     const text = getMessageText(msg)
     const userId = msg.talker().id
     if (text === null) return false
-    if (msg.talker().name() != "栋栋🤧")
+    if (msg.talker().name() != dongdongName)
         recentMessages.push({ userId, text })
     if (countVotes() >= votesNeeded) {
         kickDongdong(msg)
